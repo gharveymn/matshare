e38b6e0b02118e3359f0ea0065bc21f49156d456
Gene Harvey <gharveymn@gmail.com>
2018-03-12 10:55:08 -0500

    updates INSTALL

--- INSTALL.m
+++ INSTALL.m
@@ -1,17 +1,13 @@
-%% Run this file to compile getmatvar.
+%% Run this file to compile matshare.
 % The output is written to the 'bin' folder. 
-%
-% This program is written in the c99 standard, and so lcc is not
-% supported.
 %
 % To use getmatvar globally, navigate to the 'HOME' tab
 % on the MATLAB browser, click on 'Set Path' (underneath 'Preferences')
 % and use the 'Add Folder...' dialogue to permanantly add getmatvar
 % to your list of available functions.
 %
-% It is inadvisable to use the raw MEX function getmatvar_, so if
-% you intend to move the function elsewhere just make sure you bring
-% along getmatvar.m as well.
+% It is inadvisable to use the raw MEX function matshare_. Use the
+% provided entry functions prefixed with msh, or the MATLAB class MatShare.
 %
 % Thanks for downloading!
 %
